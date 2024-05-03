445b22becd142bfd8d0486b0c471995b69bf3765
Pedro Henrique Longo <125682292+pedrolong@users.noreply.github.com>
2024-05-03 15:18:21 -0300

    Co-authored-by: ana julia  <anaju555@users.noreply.github.com> Co-authored-by: caroline176 <caroline176@users.noreply.github.com> Co-authored-by: ProfMatheus <ProfMthLuiz@users.noreply.github.com>

--- App.js
+++ App.js
@@ -5,26 +5,44 @@ import {
   TouchableOpacity,
   Image,
 } from "react-native";
+
 import { styles } from "./Styles/Style";
 
 export default function App() {
   const imagem = {
-    uri: "https://img.freepik.com/fotos-premium/cor-de-fundo-para-apresentacoes-de-modelo-de-design-decorativo-inserir-texto-com-espaco-para-copia_7954-4083.jpg",
+    uri: "https://i.pinimg.com/474x/37/8a/96/378a96b7ef43eeae34b895087ceee556.jpg",
+  };
+  const imagem1 = {
+    uri: "https://cdn-icons-png.flaticon.com/512/2106/2106114.png",
+  };
+  const imagem2 = {
+    uri: "https://cdn-icons-png.flaticon.com/512/83/83712.png",
+  };
+  const imagem3 = {
+    uri: "https://cdn-icons-png.flaticon.com/512/4967/4967006.png",
+  };
+  const imagem4 = {
+    uri: "https://cdn-icons-png.flaticon.com/512/1020/1020719.png",
   };
 
   return (
     <View style={styles.container}>
       <Text style={styles.text}>TREK TREK TURISMOS</Text>
       <ImageBackground source={imagem} resizeMode="cover" style={styles.image}>
-        <TouchableOpacity styles={styles.button}></TouchableOpacity>
-        <TouchableOpacity styles={styles.button}></TouchableOpacity>
-        <TouchableOpacity styles={styles.button}></TouchableOpacity>
-        <TouchableOpacity styles={styles.button}></TouchableOpacity>
-        <TouchableOpacity styles={styles.button}></TouchableOpacity>
+        <TouchableOpacity style={styles.button1}>
+          <Image source={imagem1} style={styles.imgbtn} />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button2}>
+          <Image source={imagem2} style={styles.imgbtn2} />
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.button3}>
+          <Image source={imagem3} style={styles.imgbtn3} />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button4}>
+          <Image source={imagem4} style={styles.imgbtn4} />
+        </TouchableOpacity>
       </ImageBackground>
-      <View style={styles.footer}>
-        <Text style={styles.trektext}> trektrek</Text>
-      </View>
     </View>
   );
 }
